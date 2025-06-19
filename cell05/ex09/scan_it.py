00791aa49023376de5faaab1c603fddc0d0b423c
sxmmy1749 <samchatchaya1749@gmail.com>
2025-06-19 03:59:48 +0000

    addscan_it.py

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,9 @@
+import sys
+import re
+
+if len (sys.argv) ==3:
+    keyword = sys.argv[1]
+    string_to_search + sys.argv[2]
+    occurrences = re.findall(keyword, string_to_search)
+else:
+    print("none")
