91708f548bc8f2f393047ee058b65abf211b708a
sxmmy1749 <samchatchaya1749@gmail.com>
2025-06-19 03:29:17 +0000

    add.upcase_it.py

--- cell05/ex06/upcase_it.py
+++ cell05/ex06/upcase_it.py
@@ -1,1 +1,8 @@
-print("RTFM (Read the F-ing manual)")
+import sys
+def main():
+    if len(sys.argv) == 2:
+        print(sys.argv[1].upper())
+    else:
+        print("RTFM (Read the F-ing manual)")
+if __name__ == "__main__":
+    main()
