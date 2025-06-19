17f8e8425ad8139f2a45f7cc7b873f86c7baca8e
sxmmy1749 <samchatchaya1749@gmail.com>
2025-06-19 04:48:46 +0000

    addstring_are_arrays.py

---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,16 @@
+import sys 
+def main():
+    if len(sys.argv) != 2:
+        print("none")
+        return
+
+    text = sys.argv[1]
+    coun_z == text.count("z")
+
+    if count_z == 0:
+        print("none")
+    else:
+        print('z' * count_z)
+
+if __name__ == "__main__":
+    main()
